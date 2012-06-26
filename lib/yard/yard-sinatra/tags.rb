22420ddff087eef0025bcebe7c9acda18fd5b7bb
Oliver Weyhmueller <oliver@weyhmueller.de>
2012-06-26 11:45:57 +0200

    Some visual improvements regarding the request and response tags

--- lib/yard/yard-sinatra/tags.rb
+++ lib/yard/yard-sinatra/tags.rb
@@ -1,12 +1,11 @@
 # Define custom tags
 
-YARD::Tags::Library.define_tag("Request", :request)
-YARD::Tags::Library.define_tag("Response", :response)
+YARD::Tags::Library.define_tag("Request", :request, :with_title_and_text)
+YARD::Tags::Library.define_tag("Response", :response, :with_title_and_text)
 YARD::Tags::Library.define_tag("Response Fields", :response_field, :with_types_and_name)
 YARD::Tags::Library.define_tag("Request Fields", :request_field, :with_types_and_name)
 YARD::Tags::Library.define_tag("Headers", :header, :with_name)
 YARD::Tags::Library.define_tag("Response codes", :response_code, :with_name)
 YARD::Tags::Library.define_tag("Image", :image)
 YARD::Tags::Library.define_tag("Overall", :overall)
-
 YARD::Tags::Library.define_tag("Data Hash", :data,:with_options)
